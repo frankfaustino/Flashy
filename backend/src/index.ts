42f355760f3657168310221f167d366063d72e27
Frank Faustino <nawzyah@gmail.com>
2018-12-21 21:27:11 -0800

    ✨ Add auth and validation middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,28 @@
-import { ApolloServer, gql } from 'apollo-server'
-import { importSchema } from 'graphql-import'
+import { ApolloServer } from 'apollo-server'
+import { applyMiddleware } from 'graphql-middleware'
+import { makeExecutableSchema } from 'graphql-tools'
+
 import { Prisma } from './generated/prisma-client'
 import resolvers from './resolvers'
+import typeDefs from './schemas'
+import { authorization, validation } from './utils'
 
-const { APP_SECRET, PORT, PRISMA_ENDPOINT } = process.env
+export const startServer = (async () => {
+  const { PORT, PRISMA_ENDPOINT, PRISMA_SECRET } = process.env
+  const execSchema = makeExecutableSchema({ typeDefs, resolvers })
+  const schema = applyMiddleware(execSchema, authorization, validation)
 
-const prisma = new Prisma({
-  endpoint: PRISMA_ENDPOINT,
-  secret: APP_SECRET,
-  debug: true
-})
+  const prisma = new Prisma({
+    endpoint: PRISMA_ENDPOINT,
+    secret: PRISMA_SECRET,
+    debug: false
+  })
 
-const importedTypeDefs = importSchema(__dirname + '/schema.graphql')
-const typeDefs = gql`${importedTypeDefs}`
+  const server = new ApolloServer({
+    schema,
+    context: ({ req }: any) => ({ ...req, prisma })
+  })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: req => ({ ...req, prisma })
-})
-
-server
-  .listen({ port: PORT }, () => console.log(`🚀 Server is running on http://localhost:${PORT}`))
-  .catch(err => console.error(err))
+  await server.listen({ port: PORT })
+  console.log(`🚀 Server is running on http://localhost:${PORT}`)
+})()
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,28 +1,69 @@
 import { verify } from 'jsonwebtoken'
-import { Prisma } from './generated/prisma-client'
+import { object, string, ValidationError } from 'yup'
+import { Context, Token } from './types/graphql-utils'
 
-export interface Context {
-  prisma: Prisma
-  request: any
-}
-
-interface Token {
-  userId: string
-}
+// Utility functions
 
 export function getUserId(ctx: Context) {
-  const Authorization = ctx.request.get('Authorization')
+  const Authorization = ctx.headers.authorization
+
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
     const { userId } = verify(token, process.env.APP_SECRET) as Token
+
     return userId
   }
-
-  throw new AuthError()
 }
 
-export class AuthError extends Error {
-  constructor() {
-    super('Not authorized')
+export async function getPermission(ctx: Context) {
+  const id = ctx.userId
+
+  if (id) {
+    const user = ctx.prisma.user({ id })
+    const res = await user.permissions()
+
+    return res[0] === 'ADMIN'
   }
 }
+
+export function sendError(err?: ValidationError) {
+  const errors: Array<{ path: string, message: string }> = []
+  if (err) {
+    err.inner.forEach(({ path, message }) => errors.push({ path, message }))
+  } else {
+    errors.push({ path: '', message: 'Not authorized' })
+  }
+
+  return { errors }
+}
+
+const yupSchema = object().shape({
+  name: string().trim().min(2).max(255),
+  email: string().trim().min(3).max(255).email(),
+  password: string().min(3).max(255)
+})
+
+// Middleware
+
+export const validation = {
+  async Mutation (resolve: any, root: any, args: object, ctx: Context, info: any) {
+    try {
+      await yupSchema.validate(args, { abortEarly: false })
+    } catch (err) {
+      return sendError(err)
+    }
+    const result = await resolve(root, args, ctx, info)
+    return result
+  }
+}
+
+export const authorization = ['Query', 'Mutation'].reduce((obj, key) => {
+  obj[key] = async (resolve: any, root: any, args: object, ctx: Context, info: any) => {
+    const id = getUserId(ctx)
+    ctx.userId = id
+    ctx.userAdmin = await getPermission(ctx)
+    const result = await resolve(root, args, ctx, info)
+    return result
+  }
+  return obj
+}, {})
