daebc59d18848ca23f8c24230946ad13c5d496f2
Frank Faustino <nawzyah@gmail.com>
2018-12-23 21:39:50 -0800

    🎣 Add useInputValue custom hook

--- frontend/utils/index.ts
+++ frontend/utils/index.ts
@@ -1,6 +1,7 @@
 import style, { css, GlobalStyle, keyframes, theme, ThemeProvider } from './style-theme'
 import { media, truncate } from './style-utils'
+import { useInputValue } from './useInputValue'
 
 export default style
 
-export { css, GlobalStyle, keyframes, media, theme, ThemeProvider, truncate }
+export { css, GlobalStyle, keyframes, media, theme, ThemeProvider, truncate, useInputValue }
---
+++ frontend/utils/useInputValue.ts
@@ -0,0 +1,9 @@
+import { useCallback, useState } from 'react'
+
+export function useInputValue<T>(initialValue: T): [T, (e: any) => void] {
+  const [value, setValue] = useState<T>(initialValue)
+  const onChange = useCallback(event => {
+    setValue(event.currentTarget.value)
+  }, [])
+  return [value, onChange]
+}
