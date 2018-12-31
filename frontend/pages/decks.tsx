d8d412c64e00f738dc7ecf43467c5fc2b948e1e0
Frank Faustino <nawzyah@gmail.com>
2018-12-30 23:13:31 -0800

    ✨ Add folder SVG

--- frontend/pages/decks.tsx
+++ frontend/pages/decks.tsx
@@ -1,15 +1,26 @@
-import { Folder } from '../elements'
-import styled from '../lib'
+import withAuth from '../blocks/withAuth'
+import styled from '../lib/styles/global-styles'
 
 const Main = styled.div`
-  display: flex;
+  display: grid;
   flex-direction: column;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
+  grid-template-columns: 200px 200px;
+  grid-template-rows: 160px 160px;
+  grid-gap: 40px;
   height: 80vh;
+  padding-top: 50px;
   max-width: ${({ theme }) => theme.maxWidth};
 `
 
-const Decks: React.SFC = () => <Main><Folder /></Main>
+const Decks: React.SFC = () => (
+  <Main>
+    <img src="/static/folder_green.svg" />
+    <img src="/static/folder_red.svg" />
+    <img src="/static/folder_blue.svg" />
+    <img src="/static/folder_lightgrey.svg" />
+  </Main>
+)
 
-export default Decks
+export default withAuth(Decks)
