f803e2e89ad2f96009248a83f1ea9e08c8c7feee
Frank Faustino <nawzyah@gmail.com>
2018-12-30 23:17:55 -0800

    📝 Add landing page

---
+++ frontend/pages/landing.tsx
@@ -0,0 +1,32 @@
+import { Button, H1, LogoWithBg } from '../elements'
+import styled from '../lib/styles/global-styles'
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  height: 80vh;
+  max-width: ${({ theme }) => theme.maxWidth};
+`
+
+const Buttons = styled.div`
+  display: grid;
+  overflow: visible;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+  margin-top: 25px;
+`
+
+const Landing = () => (
+  <Main>
+    <LogoWithBg />
+    <H1>FLASHY</H1>
+    <Buttons>
+      <Button text="Sign up" to="/signup" variant="dashed" />
+      <Button text="Login" to="/login" variant="solid" />
+    </Buttons>
+  </Main>
+)
+
+export default Landing
