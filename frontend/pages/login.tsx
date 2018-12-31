4ba5a36d03aa800c0eebc5dbe6acffdcf34b04bb
Frank Faustino <nawzyah@gmail.com>
2018-12-30 23:18:18 -0800

    🔨 Refactor code

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,55 +1,49 @@
 import { Mutation } from 'react-apollo'
-import gql from 'graphql-tag'
 import { object, string } from 'yup'
 
 import Form, { FormLib } from '../blocks/Form'
 import { LogoWithBg, H2 } from '../elements'
-import styled from '../lib'
+import { LOGIN_MUTATION } from '../lib/graphql/mutations'
+import { ME_QUERY } from '../lib/graphql/queries'
+import styled from '../lib/styles/global-styles'
 
 interface InputValues {
   email: string
   password: string
 }
 
-const LOGIN_MUTATION = gql`
-  mutation LOGIN_MUTATION($email: String!, $password: String!) {
-    logIn(email: $email, password: $password) {
-      id
-      token
-    }
-  }
-`
-
 const Main = styled.div`
   display: grid;
   grid-auto-rows: 178px 100px 1fr;
   justify-content: center;
   align-items: center;
+  height: 80vh;
 `
 
-const LogInPage = () => {
-  const schema = object().shape({
-    email: string().trim().min(3).max(255).email().required(),
-    password: string().min(3).max(255).required()
-  })
+const schema = object().shape({
+  email: string().trim().min(3).max(255).email().required(),
+  password: string().min(3).max(255).required()
+})
 
-  return (
-    <Main>
-      <LogoWithBg />
-      <H2>Log in to Flashy!</H2>
-      <Mutation mutation={LOGIN_MUTATION}>
-        {(logIn, { loading }) => (
-          <FormLib
-          initialValues={{ email: '', password: '' }}
-          validate={(values: InputValues) => schema.validateSync(values, { abortEarly: false })}
-          onSubmit={logIn}
-        >
-          <Form type="Log in" loading={loading} />
-        </FormLib>
-        )}
+const LoginMutation = (logIn, { loading }) => (
+  <FormLib
+    initialValues={{ email: '', password: '' }}
+    validate={(values: InputValues) => schema.validateSync(values, { abortEarly: false })}
+    onSubmit={logIn}
+  >
+    <Form type="Log in" loading={loading} />
+  </FormLib>
+)
+
+
+const LogIn = () => (
+  <Main>
+    <LogoWithBg />
+    <H2>Log in to Flashy!</H2>
+      <Mutation mutation={LOGIN_MUTATION} refetchQueries={[{ query: ME_QUERY }]}>
+        {LoginMutation}
       </Mutation>
-    </Main>
-  )
-}
+  </Main>
+)
 
-export default LogInPage
+export default LogIn
--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,10 +1,11 @@
 import { Mutation } from 'react-apollo'
-import gql from 'graphql-tag'
 import { object, string } from 'yup'
 
 import Form, { FormLib } from '../blocks/Form'
 import { LogoWithBg, H2 } from '../elements'
-import styled from '../lib'
+import { SIGNUP_MUTATION } from '../lib/graphql/mutations'
+import { ME_QUERY } from '../lib/graphql/queries'
+import styled from '../lib/styles/global-styles'
 
 interface InputValues {
   email: string
@@ -12,50 +13,38 @@ interface InputValues {
   password: string
 }
 
-const SIGNUP_MUTATION = gql`
-  mutation SIGNUP_MUTATION(
-    $name: String!
-    $email: String!
-    $password: String!
-  ) {
-    signUp(name: $name, email: $email, password: $password) {
-      id
-      token
-    }
-  }
-`
-
 const Main = styled.div`
   display: grid;
   grid-auto-rows: 178px 100px 1fr;
   justify-content: center;
   align-items: center;
+  height: 80vh;
 `
 
-const SignUp = () => {
-  const schema = object().shape({
-    name: string().trim().min(2).max(255).required(),
-    email: string().trim().min(3).max(255).email().required(),
-    password: string().min(3).max(255).required()
-  })
+const schema = object().shape({
+  name: string().trim().min(2).max(255).required(),
+  email: string().trim().min(3).max(255).email().required(),
+  password: string().min(3).max(255).required()
+})
 
-  return (
-    <Main>
-      <LogoWithBg />
-      <H2>Sign up for Flashy!</H2>
-      <Mutation mutation={SIGNUP_MUTATION}>
-        {(signUp, { loading }) => (
-          <FormLib
-            initialValues={{ email: '', name: '', password: '' }}
-            validate={(values: InputValues) => schema.validateSync(values, { abortEarly: false })}
-            onSubmit={signUp}
-          >
-            <Form type="Sign up" loading={loading} />
-          </FormLib>
-        )}
-      </Mutation>
-    </Main>
-  )
-}
+const SignUpMutation = (signUp, { loading }) => (
+  <FormLib
+    initialValues={{ email: '', name: '', password: '' }}
+    validate={(values: InputValues) => schema.validateSync(values, { abortEarly: false })}
+    onSubmit={signUp}
+  >
+    <Form type="Sign up" loading={loading} />
+  </FormLib>
+)
+
+const SignUp = () => (
+  <Main>
+    <LogoWithBg />
+    <H2>Sign up for Flashy!</H2>
+    <Mutation mutation={SIGNUP_MUTATION} refetchQueries={[{ query: ME_QUERY }]}>
+      {SignUpMutation}
+    </Mutation>
+  </Main>
+)
 
 export default SignUp
